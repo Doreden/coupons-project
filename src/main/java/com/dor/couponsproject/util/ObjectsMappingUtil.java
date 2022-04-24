84b66329eb8f314e51274dc6b73beedd037fa009
Emil Bugay <emilbu9@gmail.com>
2022-04-24 18:39:13 +0300

    A new change to ObjectsMappingUtil class

--- src/main/java/com/dor/couponsproject/util/ObjectsMappingUtil.java
+++ src/main/java/com/dor/couponsproject/util/ObjectsMappingUtil.java
@@ -1,11 +1,8 @@
 package com.dor.couponsproject.util;
 
-import com.dor.couponsproject.entities.CompanyEntity;
 import lombok.Builder;
-
-import java.util.Optional;
 
 @Builder
 public class ObjectsMappingUtil {
-
+//HAYDE
 }
